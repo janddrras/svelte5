5dddca795b22891b24701a6c7b3743f1143b491a
janddrras <janddrras@gmail.com>
2024-10-25 13:16:31 +0300

    timer

---
+++ src/lib/components/Timer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let elapsed = $state(0)
+	let duration = $state(1)
+
+	let interval: number
+
+	function start() {
+		interval = setInterval(() => {
+			elapsed += 0.1
+			if (elapsed > duration) clearInterval(interval)
+		}, 100)
+	}
+	function reset() {
+		elapsed = 0
+		clearInterval(interval)
+	}
+</script>
+
+<div class="w-full flex items-center justify-center gap-2">
+	<h2>Timer</h2>
+	<label class="flex flex-col gap-2">
+		<span>elapsed time:</span>
+		<progress max={duration} value={elapsed}></progress>
+	</label>
+	<p class="px-4">{elapsed.toFixed(1)} s</p>
+	<label class="flex flex-col gap-2">
+		<span>duration:</span>
+		<input type="range" bind:value={duration} min="1" max="10" />
+	</label>
+	<p class="px-4">{duration} s</p>
+	<button class="bg-blue-500 text-white px-4 py-2 rounded" onclick={start}>Start</button>
+	<button class="bg-blue-500 text-white px-4 py-2 rounded" onclick={reset}>Reset</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from '$lib/components/Counter.svelte'
 	import FlightBooker from '$lib/components/FlightBooker.svelte'
 	import TempConverter from '$lib/components/TempConverter.svelte'
+	import Timer from '$lib/components/Timer.svelte'
 </script>
 
 <h1 class="text-2xl pb-14">Welcome to SvelteKit</h1>
@@ -11,4 +12,6 @@
 	<TempConverter />
 	<hr class="m-14 border-t-slate-600" />
 	<FlightBooker />
+	<hr class="m-14 border-t-slate-600" />
+	<Timer />
 </div>
